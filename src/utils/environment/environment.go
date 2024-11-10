1f971403275a21f6b651021a39fc32ec0fb8c9ad
oagarian <tns6@aluno.ifal.edu.br>
2024-11-10 12:34:21 -0300

    fix: environment

--- src/utils/environment/environment.go
+++ src/utils/environment/environment.go
@@ -8,21 +8,24 @@ import (
 type environment struct {
     GeminiToken string
     DatabaseURL string
-    JWTSecret string
+    JWTSecret   string
 }
 
 func load() {
-    err := godotenv.Load()
-    if err != nil {
-        panic("Error loading .env file")
-    }
+	if os.Getenv("GO_ENV") == "development" {
+		err := godotenv.Load()
+		if err != nil {
+			panic("Error loading .env file")
+		}
+	}
 }
 
+// Função para obter as variáveis de ambiente
 func Get() (environment, error) {
-    load()
-    return environment{
-        GeminiToken:   os.Getenv("GEMINI_TOKEN"),
-        DatabaseURL: os.Getenv("DATABASE_URL"),
-        JWTSecret: os.Getenv("JWT_SECRET"),
-    }, nil
+	load()  
+	return environment{
+		GeminiToken: os.Getenv("GEMINI_TOKEN"),
+		DatabaseURL: os.Getenv("DATABASE_URL"),
+		JWTSecret:   os.Getenv("JWT_SECRET"),
+	}, nil
 }
